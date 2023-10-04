428f6ee0a0bad1bf719e07e7899e78127ff18bed
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 16:30:28 +0200

    Changed BottleSong to return string instead of printing

--- bottles.py
+++ bottles.py
@@ -1,12 +1,22 @@
-def bottle_song():
-	user_input = int(input("How many bottles do you want to start with?"))
-	count = user_input
-	while count > 1:
 
-		print(f"{count} bottles of beer on the wall, {count} bottles of beer. Take one down and pass it around, {count-1} {'bottles' if count > 2 else 'bottle' } of beer on the wall.")
-		count = count -1
 
-	print(f"Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, {user_input} bottles of beer on the wall.")
+def bottle_song(num_bottles):
+	"""
+	Returns a string with the # of bottles of beer on the wall
+	Arguments:
+	num_bottles - int - Number of bottles the song should start with.
+	"""
+	bottle_song = ""
+	orginial_bottles = num_bottles
+	while num_bottles > 1:
+
+		bottle_song += f"{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer. Take one down and pass it around, {num_bottles-1} {'bottles' if num_bottles > 2 else 'bottle' } of beer on the wall.\n"
+		num_bottles = num_bottles -1
+
+	bottle_song += f"Take one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, {orginial_bottles} bottles of beer on the wall."
 	pass
 
-bottle_song()
+
+
+bottles_user_input = int(input("How many bottles do you want to start with?"))
+print(bottle_song(bottles_user_input))
