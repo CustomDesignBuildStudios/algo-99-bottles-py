b2e1d3f7c4b201706564f530c93f83c75a3bfbee
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 16:11:50 +0200

    Added 99Bottle Code for BattleSong function

--- bottles.py
+++ bottles.py
@@ -1,5 +1,12 @@
 def bottle_song():
-	# write your code here!
+	user_input = int(input("How many bottles do you want to start with?"))
+	count = user_input
+	while count > 1:
+
+		print(f"{count} bottles of beer on the wall, {count} bottles of beer. Take one down and pass it around, {count-1} {'bottles' if count > 2 else 'bottle' } of beer on the wall.")
+		count = count -1
+
+	print(f"Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, {user_input} bottles of beer on the wall.")
 	pass
 
 bottle_song()
